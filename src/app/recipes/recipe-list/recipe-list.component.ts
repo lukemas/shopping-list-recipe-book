0049ee1e5e0cbcaa7ee9dab8834db37c66f8c3f8
lukemas <l.maslowski91@gmail.com>
2018-12-29 08:26:39 +0100

    recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('jajuwa', 'wbic jaja i usmazyc', 'https://www.zajadam.pl/wp-content/uploads/2015/11/jajecznica-3-2-891x500.jpg' )
+];
 
   constructor() { }
 
